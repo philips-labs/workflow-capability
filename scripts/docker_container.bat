73b9700aa38eb9ca1437361ba207e58386a38719
Filomijn van der Poel <71440458+amazingfilo@users.noreply.github.com>
2023-04-11 12:00:09 +0200

    small fix

--- scripts/docker_container.bat
+++ scripts/docker_container.bat
@@ -1,2 +1,4 @@
 cd..
+docker run -p 5432:5432 -e POSTGRES_PASSWORD=admin -d postgres
+timeout /T 5
 docker run -p 8180:8080 -v "%cd%/fhir_jpa_config:/data" -e "--spring.config.location=file:///data/application.yaml" hapiproject/hapi:latest
