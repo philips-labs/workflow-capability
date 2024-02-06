85c43b12ca1eccf09517e6647282df3af518d7d8
tesfayegec <132367111+tesfayegec@users.noreply.github.com>
2024-02-06 11:06:31 +0100

    initial commit

--- workflow_capability_core/src/main/java/com/philips/healthsuite/workflowcapability/core/WfcServiceApplication.java
+++ workflow_capability_core/src/main/java/com/philips/healthsuite/workflowcapability/core/WfcServiceApplication.java
@@ -1,6 +1,8 @@
 package com.philips.healthsuite.workflowcapability.core;
-
 import com.philips.healthsuite.workflowcapability.core.demos.*;
+
+import org.apache.jena.base.Sys;
+import org.springframework.beans.BeansException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -13,88 +15,100 @@ import java.util.List;
 public class WfcServiceApplication {
 
     public static void main(String[] args) {
-
-        if (args.length > 0) {
-            AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext()) {
             context.scan("com.philips.healthsuite.workflowcapability.core.demos");
             context.refresh();
             FhirStoreInitialization fhirStoreInitialization = context.getBean(FhirStoreInitialization.class);
-            fhirStoreInitialization.run();
-            List<String> argsList = List.of(args);
-            if (argsList.contains("withAllDemos")) {
-                System.out.println("Running Preprocessing Step (all demos)");
-                context.getBean(DemoCreator1.class).run();
-                context.getBean(DemoCreator2.class).run();
-                context.getBean(DemoCreator3.class).run();
-                context.getBean(DemoCreator4.class).run();
-                context.getBean(DemoCreator5.class).run();
-                context.getBean(DemoCreator6.class).run();
-                context.getBean(DemoCreator7.class).run();
-                context.getBean(DemoCreator8.class).run();
-                context.getBean(DemoCreator9.class).run();
-                context.getBean(DemoCreator10.class).run();
-            } else if (argsList.contains("withSepsisV2Demos")) {
-                System.out.println("Running Preprocessing Step (sepsis v2 demos)");
-                context.getBean(DemoCreator5.class).run();
-                context.getBean(DemoCreator6.class).run();
-                context.getBean(DemoCreator7.class).run();
-                context.getBean(DemoCreator8.class).run();
-                context.getBean(DemoCreator9.class).run();
-                context.getBean(DemoCreator10.class).run();
-            } else {
-                if (argsList.contains("withPreprocessing1")) {
-                    System.out.println("Running Preprocessing Step (demo data 1)");
-                    DemoCreator1 ms = context.getBean(DemoCreator1.class);
-                    ms.run();
-                }
-                if (argsList.contains("withPreprocessing2")) {
-                    System.out.println("Running Preprocessing Step (demo data 2)");
-                    DemoCreator2 ms = context.getBean(DemoCreator2.class);
-                    ms.run();
-                }
-                if (argsList.contains("withPreprocessing3")) {
-                    System.out.println("Running Preprocessing Step (demo data 3)");
-                    DemoCreator3 ms = context.getBean(DemoCreator3.class);
-                    ms.run();
-                }
-                if (argsList.contains("withPreprocessing4")) {
-                    System.out.println("Running Preprocessing Step (demo data 4)");
-                    DemoCreator4 ms = context.getBean(DemoCreator4.class);
-                    ms.run();
-                }
-                if (argsList.contains("withPreprocessing5")) {
-                    System.out.println("Running Preprocessing Step (demo data 5)");
-                    DemoCreator5 ms = context.getBean(DemoCreator5.class);
-                    ms.run();
-                }
-                if (argsList.contains("withPreprocessing6")) {
-                    System.out.println("Running Preprocessing Step (demo data 6)");
-                    DemoCreator6 ms = context.getBean(DemoCreator6.class);
-                    ms.run();
-                }
-                if (argsList.contains("withPreprocessing7")) {
-                    System.out.println("Running Preprocessing Step (demo data 7)");
-                    DemoCreator7 ms = context.getBean(DemoCreator7.class);
-                    ms.run();
-                }
-                if (argsList.contains("withPreprocessing8")) {
-                    System.out.println("Running Preprocessing Step (demo data 8)");
-                    DemoCreator8 ms = context.getBean(DemoCreator8.class);
-                    ms.run();
-                }
-                if (argsList.contains("withPreprocessing9")) {
-                    System.out.println("Running Preprocessing Step (demo data 9)");
-                    DemoCreator9 ms = context.getBean(DemoCreator9.class);
-                    ms.run();
-                }
-                if (argsList.contains("withPreprocessing10")) {
-                    System.out.println("Running Preprocessing Step (demo data 10)");
-                    DemoCreator10 ms = context.getBean(DemoCreator10.class);
-                    ms.run();
+            if (args.length > 0) {
+                List<String> argsList = List.of(args);
+                if (argsList.contains("withAllDemos")) {
+                    System.out.println("Running Preprocessing Step (all demos)");
+                    context.getBean(DemoCreator1.class).run();
+                    context.getBean(DemoCreator2.class).run();
+                    context.getBean(DemoCreator3.class).run();
+                    context.getBean(DemoCreator4.class).run();
+                    context.getBean(DemoCreator5.class).run();
+                    context.getBean(DemoCreator6.class).run();
+                    context.getBean(DemoCreator7.class).run();
+                    context.getBean(DemoCreator8.class).run();
+                    context.getBean(DemoCreator9.class).run();
+                    context.getBean(DemoCreator10.class).run();
+                } else if (argsList.contains("withSepsisV2Demos")) {
+                    System.out.println("Running Preprocessing Step (sepsis v2 demos)");
+                    context.getBean(DemoCreator5.class).run();
+                    context.getBean(DemoCreator6.class).run();
+                    context.getBean(DemoCreator7.class).run();
+                    context.getBean(DemoCreator8.class).run();
+                    context.getBean(DemoCreator9.class).run();
+                    context.getBean(DemoCreator10.class).run();
+                } else {
+                    if (argsList.contains("withPreprocessing1")) {
+                        System.out.println("Running Preprocessing Step (demo data 1)");
+                        DemoCreator1 ms = context.getBean(DemoCreator1.class);
+                        ms.run();
+                    }
+                    if (argsList.contains("withPreprocessing2")) {
+                        System.out.println("Running Preprocessing Step (demo data 2)");
+                        DemoCreator2 ms = context.getBean(DemoCreator2.class);
+                        ms.run();
+                    }
+                    if (argsList.contains("withPreprocessing3")) {
+                        System.out.println("Running Preprocessing Step (demo data 3)");
+                        DemoCreator3 ms = context.getBean(DemoCreator3.class);
+                        ms.run();
+                    }
+                    if (argsList.contains("withPreprocessing4")) {
+                        System.out.println("Running Preprocessing Step (demo data 4)");
+                        DemoCreator4 ms = context.getBean(DemoCreator4.class);
+                        ms.run();
+                    }
+                    if (argsList.contains("withPreprocessing5")) {
+                        System.out.println("Running Preprocessing Step (demo data 5)");
+                        DemoCreator5 ms = context.getBean(DemoCreator5.class);
+                        ms.run();
+                    }
+                    if (argsList.contains("withPreprocessing6")) {
+                        System.out.println("Running Preprocessing Step (demo data 6)");
+                        DemoCreator6 ms = context.getBean(DemoCreator6.class);
+                        ms.run();
+                    }
+                    if (argsList.contains("withPreprocessing7")) {
+                        System.out.println("Running Preprocessing Step (demo data 7)");
+                        DemoCreator7 ms = context.getBean(DemoCreator7.class);
+                        ms.run();
+                    }
+                    if (argsList.contains("withPreprocessing8")) {
+                        System.out.println("Running Preprocessing Step (demo data 8)");
+                        DemoCreator8 ms = context.getBean(DemoCreator8.class);
+                        ms.run();
+                    }
+                    if (argsList.contains("withPreprocessing9")) {
+                        System.out.println("Running Preprocessing Step (demo data 9)");
+                        DemoCreator9 ms = context.getBean(DemoCreator9.class);
+                        ms.run();
+                    }
+                    if (argsList.contains("withPreprocessing10")) {
+                        System.out.println("Running Preprocessing Step (demo data 10)");
+                        DemoCreator10 ms = context.getBean(DemoCreator10.class);
+                        ms.run();
+                    }
                 }
             }
-        }
 
+            // FhirStoreInitialization fhirStoreInitialization = context.getBean(FhirStoreInitialization.class);
+            // fhirStoreInitialization.run();
+            else{
+                System.err.println("No arguments provided. Running with default preprocessing step" );
+                fhirStoreInitialization.run();
+                System.out.println("Preprocessing step completed");
+            }
+        
+        } catch (BeansException | IllegalStateException e) {
+            System.out.println("An error occured while running the preprocessing step");
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        System.setProperty("java.net.preferIPv4Stack", "true");
         SpringApplication.run(WfcServiceApplication.class, args);
     }
 
--- workflow_capability_core/src/main/java/com/philips/healthsuite/workflowcapability/core/demos/FhirStoreInitialization.java
+++ workflow_capability_core/src/main/java/com/philips/healthsuite/workflowcapability/core/demos/FhirStoreInitialization.java
@@ -1,6 +1,8 @@
 package com.philips.healthsuite.workflowcapability.core.demos;
 
 import com.philips.healthsuite.workflowcapability.core.fhirresources.FhirDataResources;
+
+import org.apache.jena.base.Sys;
 import org.hl7.fhir.r4.model.Subscription;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
@@ -22,6 +24,7 @@ public class FhirStoreInitialization {
 
 
     public void run() {
+        System.out.println("Running FhirStoreInitialization");
         this.wfcUrl = this.env.getProperty("config.wfcUrl");
         this.fhirUrl = this.env.getProperty("config.fhirUrl");
         this.fhirDataResources = new FhirDataResources(this.fhirUrl + "/fhir");
@@ -32,6 +35,7 @@ public class FhirStoreInitialization {
 
 
     public void addTaskSubscription() {
+        System.out.println("Adding Task Subscription");
         Subscription taskSubscription = new Subscription();
         taskSubscription.setStatus(Subscription.SubscriptionStatus.REQUESTED);
         taskSubscription.setReason("Trigger when a Task is completed");
@@ -44,6 +48,7 @@ public class FhirStoreInitialization {
 
 
     public void addCarePlanSubscription() {
+        System.out.println("Adding CarePlan Subscription");
         Subscription carePlanSubscription = new Subscription();
         carePlanSubscription.setStatus(Subscription.SubscriptionStatus.REQUESTED);
         carePlanSubscription.setReason("Trigger when a new CarePlan is created");
